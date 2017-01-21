9289bdda937e9b94c69155c3e8b00e057c413a65
Administrator <macuser@Macintosh-3.local>
2017-01-21 14:12:15 -0600

    Started adding events listing to camps

--- Tabbed App Demo/CampsDetailViewController.swift
+++ Tabbed App Demo/CampsDetailViewController.swift
@@ -23,7 +23,7 @@ class CampsDetailViewController: UIViewController {
         super.viewDidLoad()
         
         self.configureView()
-        
+        self.findEvents()
 
         // Do any additional setup after loading the view.
     }
@@ -55,6 +55,85 @@ class CampsDetailViewController: UIViewController {
                 hloc.text = detailh
             }
         }
+        
+    }
+    
+        func findEvents() {
+            var objects = [String]()
+            
+            var TableData:Array<String> = Array <String>()
+            var events: [String] = []
+            var eventsDesc: [String] = []
+            var eventsLoc: [String] = []
+            var eventsHLoc: [String] = []
+            var eventsDay: [String] = []
+            var eventsStart: [String] = []
+            var eventsEnd: [String] = []
+            var aObject: [String:AnyObject] = [:]
+            
+            
+            if let path = Bundle.main.path(forResource: "events2", ofType: "json") {
+                
+                let url = URL(fileURLWithPath: path)
+                
+                do {
+                    
+                    let allEventsData = try Data(contentsOf: url)
+                    
+                    let allEvents = try JSONSerialization.jsonObject(with: allEventsData, options: JSONSerialization.ReadingOptions.allowFragments) as! [String : AnyObject]
+                    
+                    //               print(allEvents)
+                    
+                    
+                    if let arrJSON = allEvents["Events"] {
+                        
+                        
+                        for index in 0...arrJSON.count-1 {
+
+                            
+                            aObject = arrJSON[index] as! [String : AnyObject]
+
+                            
+                            if (aObject["Location"] as! String) == "Canned Ham Camp" {
+                                print(aObject["Title"] as! String)
+
+                            
+                            events.append(aObject["Title"] as! String)
+                            eventsDesc.append(aObject["Description"] as! String)
+                            eventsLoc.append(aObject["Location"] as! String)
+                            eventsHLoc.append(aObject["HumanLocation"] as! String)
+                            eventsDay.append(aObject["Day"] as! String)
+                            eventsStart.append(aObject["StartTime"] as! String)
+                            eventsEnd.append(aObject["EndTime"] as! String)
+
+                            
+                            }
+                            
+                            
+                            
+                            
+                        }
+                        
+                        
+                        
+                    }
+                    
+                    
+                }
+                catch {
+                    
+                }
+                
+
+            
+        }
+        
+        
+        
+        
+        
+        
+        
         
     }
     
