e10af4d2d5a2a5d0a52623d56e55ef98bb7894d7
Administrator <macuser@Macintosh-3.local>
2017-02-10 23:01:41 -0600

    Added map and notes

---
+++ Tabbed App Demo/WWWViewController.swift
@@ -0,0 +1,43 @@
+//
+//  WWWViewController.swift
+//  Tabbed App Demo
+//
+//  Created by Administrator on 2/10/17.
+//  Copyright © 2017 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class WWWViewController: UIViewController {
+
+    @IBOutlet weak var wwwWebView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        if let pdf = Bundle.main.url(forResource: "LOF-Program2016-web", withExtension: "pdf", subdirectory: nil, localization: nil)  {
+            let req = NSURLRequest(url: pdf)
+            wwwWebView.loadRequest(req as URLRequest)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
