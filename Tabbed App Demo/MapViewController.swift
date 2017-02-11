62ae44412854e1f5a4ad6d040a7f5c4724b0f8e6
Administrator <macuser@Macintosh-3.local>
2017-02-11 10:36:12 -0600

    Adding working scrolling map

---
+++ Tabbed App Demo/MapViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MapViewController.swift
+//  Tabbed App Demo
+//
+//  Created by Administrator on 2/11/17.
+//  Copyright © 2017 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    var scrollView: UIScrollView!
+    var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView = UIImageView(image: UIImage(named: "LOF-Program2016-map.jpg"))
+        
+        scrollView = UIScrollView(frame: view.bounds)
+        scrollView.backgroundColor = UIColor.black
+        scrollView.contentSize = imageView.bounds.size
+        scrollView.autoresizingMask = [ .flexibleWidth, .flexibleHeight ]
+        
+        scrollView.addSubview(imageView)
+        view.addSubview(scrollView)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
